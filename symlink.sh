0d6dccb324962ce6fbfa38ae7665471566b5a7b7
Muhammad Rijal Dwi Rozaki <rozakidwi@gmail.com>
2026-06-10 06:25:25 +0700

    update symlink.sh for sway setup.

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.config/mpd ~/.config/mpd
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sfn ~/dotfiles/.config/mpd ~/.config/mpd
+ln -sfn ~/dotfiles/.config/sway ~/.config/sway
+ln -sfn ~/dotfiles/.config/foot ~/.config/foot
+ln -sfn ~/dotfiles/.config/fuzzel ~/.config/fuzzel
+ln -sfn ~/dotfiles/.config/mako ~/.config/mako
+ln -sfn ~/dotfiles/.bashrc ~/.bashrc
