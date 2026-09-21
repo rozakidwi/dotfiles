d694ad931e4e6deeb887932996d97f731b889b03
Muhammad Rijal Dwi Rozaki <rozakidwi@gmail.com>
2026-09-21 16:10:22 +0700

    nvim: configure basic settings
    the default unconfigured experience is decent, but these
    configurations are like QoL to me.

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,9 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.undofile = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.inccommand = 'split'
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
