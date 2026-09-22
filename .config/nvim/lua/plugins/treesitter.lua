9dd7e05692a60198e8841af719fe15fca985fed8
Muhammad Rijal Dwi Rozaki <rozakidwi@gmail.com>
2026-09-22 16:35:16 +0700

    nvim: upgrade plugins to their latest version
    i went from neovim v10 to v12.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,14 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = 'master',
+	'nvim-treesitter/nvim-treesitter',
 	lazy = false,
-	build = ":TSUpdate",
+	build = ':TSUpdate',
 	config = function()
-		require'nvim-treesitter.configs'.setup {
-			ensure_installed = {
-				"c", "lua", "vim", "vimdoc", "query",
-				"markdown", "markdown_inline",
-				"git_config", "git_rebase",
-				"gitattributes", "gitcommit",
-				"gitignore"
-			},
-			auto_install = true
+		require('nvim-treesitter').install {
+			"c", "lua", "vim", "vimdoc", "query",
+			"markdown", "markdown_inline",
+			"git_config", "git_rebase",
+			"gitattributes", "gitcommit",
+			"gitignore"
 		}
 	end
 }
