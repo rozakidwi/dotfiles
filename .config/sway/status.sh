d12a16e0e10565220aed7bf6770afd535aac26ad
Muhammad Rijal Dwi Rozaki <rozakidwi@gmail.com>
2026-09-26 06:09:26 +0700

    sway: Refactor status.sh's seperator glyph
    After checking the script to shellcheck, I realized I don't have to put
    the seperator glyph into a variable.
    
    Now, the shell script complies shellcheck's standard properly.

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-SEPERATOR="$(echo '|')"
 CLOCK="$(date +%R) WIB"
 DATE="$(date +'%a, %b %d %Y')"
 BATTERY="BAT $(cat /sys/class/power_supply/BAT?/capacity)%"
 VOLUME="VOL $(wpctl get-volume @DEFAULT_SINK@ | awk '{print $2 * 100}')%"
 
-echo "$VOLUME $SEPERATOR $BATTERY $SEPERATOR $DATE $SEPERATOR $CLOCK"
+echo "$VOLUME | $BATTERY | $DATE | $CLOCK"
